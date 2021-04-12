3c11841d488a528f2032f178f3daa640b24d9fc7
Danil Gashigullin <dgash2201@yandex.ru>
2021-04-13 00:36:57 +0300

    Initial commit

---
+++ RedisChat/Chat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisChat
+{
+    class Chat
+    {
+        private readonly string _currentUser;
+        private readonly string _companion;
+        private ConnectionMultiplexer _redis;
+        private readonly string _forthChannel;
+        private readonly string _backChannel;
+        private readonly ISubscriber _subscriber;
+        private readonly string _messagesHistoryKey;
+
+        public Chat(string currentUser, string companion, ConnectionMultiplexer redis, Action<RedisValue> responseHandler)
+        {
+            _currentUser = currentUser;
+            _companion = companion;
+            _redis = redis;
+            _forthChannel = $"channel:{_currentUser + _companion}";
+            _backChannel = $"channel:{_companion + _currentUser}";
+            _messagesHistoryKey = $"{_currentUser}-{_companion}";
+
+            _subscriber = _redis.GetSubscriber();
+            _subscriber.Subscribe(_backChannel, (channel, message) =>
+            {
+                responseHandler(message);
+                SaveMessage(message);
+            });
+        }
+
+        public void Send(string message)
+        {
+            SaveMessage($"{_currentUser}:{message}");
+            _subscriber.Publish(_forthChannel, $"{_currentUser}:{message}");
+        }
+        
+        public RedisValue[] GetMessageHistory()
+        {
+            var db = _redis.GetDatabase();
+            return db.ListRange(_messagesHistoryKey);
+        }
+
+        private void SaveMessage(string message)
+        {
+            var db = _redis.GetDatabase();
+            db.ListRightPush(_messagesHistoryKey, message);
+        }
+    }
+}
---
+++ RedisChat/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using StackExchange.Redis;
+
+namespace RedisChat
+{
+    class Program
+    {
+        private const string _exitCommand = "/exit";
+        private const ConsoleColor _defaultColor = ConsoleColor.Gray;
+        private const ConsoleColor _userColor = ConsoleColor.Green;
+        private const ConsoleColor _companionColor = ConsoleColor.Red;
+
+        static void PrintResponse(RedisValue message)
+        {
+            Console.ForegroundColor = _companionColor;
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = _defaultColor;
+        }
+
+        static void PrintMessageHistory(RedisValue[] messageHistory, string userName)
+        {
+            foreach (var value in messageHistory)
+            {
+                var sender = value.ToString().Split(":")[0];
+
+                Console.ForegroundColor = sender == userName ? _userColor : _companionColor;
+                Console.WriteLine(value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationOptions();
+            configuration.EndPoints.Add("localhost", 6379);
+
+            var redis = ConnectionMultiplexer.Connect(configuration);
+
+            Console.WriteLine("Введите имя пользователя: ");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Введите имя собеседника: ");
+            var companionName = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"Чат c {companionName}");
+            Console.WriteLine($"Для выхода из чата наберите {_exitCommand}");
+            Console.WriteLine();
+
+            var chat = new Chat(userName, companionName, redis, PrintResponse);
+            var messageHistory = chat.GetMessageHistory();
+
+            PrintMessageHistory(messageHistory, userName);
+
+            while (true)
+            {
+                Console.ForegroundColor = _userColor;
+                var toSend = Console.ReadLine();
+
+                if (toSend == _exitCommand) break;
+
+                chat.Send(toSend);
+            }
+        }
+    }
+}
